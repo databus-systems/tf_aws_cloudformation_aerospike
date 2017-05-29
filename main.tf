304c4611b6e2f7f35410e1402bf4e53d1aadada0
Valentin Bud <valentin.bud@databus.systems>
2017-05-29 09:49:00 +0300

    Added cli53 parameters and updated variables.

--- main.tf
+++ main.tf
@@ -24,7 +24,6 @@ resource "aws_cloudformation_stack" "aerospike_existing_vpc" {
 
     parameters {
         KeyPair = "${var.keypair}"
-        PermitSSH = "${var.permitssh}"
         NamespaceFile = "${var.namespacefile}"
         VPC = "${var.vpc_id}"
         VPCSubnet = "${var.vpc_subnet_id}"
@@ -33,5 +32,15 @@ resource "aws_cloudformation_stack" "aerospike_existing_vpc" {
         EBS = "${var.ebs}"
         Cloudwatch = "${var.cloudwatch}"
         Tenancy = "${var.tenancy}"
+        Cli53AwsAccessKey = "${var.secret_cli53_aws_access_key}"
+        Cli53AwsSecretAccessKey = "${var.secret_cli53_aws_secret_access_key}"
+        Cli53PrivateZone = "${var.private_zone}"
+        Cli53PublicZone = "${var.public_zone}"
+        Cli53Hostname = "${var.aerospike_cli53_hostname}"
+        ClusterRoleArn = "${var.aerospike_cluster_role_arn}"
+        ClusterAMI = "${var.aerospike_ami}"
+        ClusterInstanceProfileArn = "${var.aerospike_cluster_instance_profile_arn}"
+        MigrationSQSArn = "${var.aerospike_migration_queue_arn}"
+        SecurityGroupIDs = "${var.aerospike_security_group_ids}"
     }
 }
--- variables.tf
+++ variables.tf
@@ -5,22 +5,34 @@ variable "keypair" {}
 variable "permitssh" { default = "0.0.0.0/0" }
 variable "namespacefile" { default = "https://s3.amazonaws.com/nonexistentbucket/namespace.conf"}
 variable "new_vpc" {
-	description = "When set to true (default) deploys Aerospike in a new VPC"
-	default = true
+    description = "When set to true (default) deploys Aerospike in a new VPC"
+    default = true
 }
 variable "vpc_id" { default = "vpc-0000000"}
 variable "vpc_subnet_id" { default = "subnet-0000000"}
 variable "number_of_instances" { default = "2" }
 variable "instance_type" { default = "t2.micro" }
 variable "cloudwatch" {
-	description = "Add basic Aerospike metrics to Cloudwatch. Will incur Cloudwatch expenses ~ $24/mo/instance"
-	default = "no"
+    description = "Add basic Aerospike metrics to Cloudwatch. Will incur Cloudwatch expenses ~ $24/mo/instance"
+    default = "no"
 }
 variable "tenancy" {
-	description = "The tenancy of your instance"
-	default = "default"
+    description = "The tenancy of your instance"
+    default = "default"
 }
 variable "ebs" {
-	description = "Size of EBS SSD volume in GB. Defaults to disabled."
-	default = "0"
+    description = "Size of EBS SSD volume in GB. Defaults to disabled."
+    default = "0"
 }
+
+variable "secret_cli53_aws_access_key" {}
+variable "secret_cli53_aws_secret_access_key" {}
+variable "private_zone" {}
+variable "public_zone" {}
+variable "aerospike_cli53_hostname" {}
+
+variable "aerospike_cluster_role_arn" {}
+variable "aerospike_cluster_instance_profile_arn" {} 
+variable "aerospike_migration_queue_arn" {}
+variable "aerospike_security_group_ids" {}
+variable "aerospike_ami" {}
